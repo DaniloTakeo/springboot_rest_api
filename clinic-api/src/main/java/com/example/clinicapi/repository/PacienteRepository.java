24ccef100395562ed337cd67ad42e967199cde77
Danilo Takeo <danilot.kanizawa@gmail.com>
2025-04-26 12:18:16 -0300

    Implementação das interfaces de Repository com SpringJPA

---
+++ clinic-api/src/main/java/com/example/clinicapi/repository/PacienteRepository.java
@@ -0,0 +1,11 @@
+package com.example.clinicapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.clinicapi.model.Paciente;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+	
+}
