cf01c3b5addd074aaaefb33e4547a2e85e2f453d
Danilo Takeo <danilot.kanizawa@gmail.com>
2025-04-28 09:21:54 -0300

    Atualização das repositories com métodos de consultas customizadas

--- clinic-api/src/main/java/com/example/clinicapi/repository/MedicoRepository.java
+++ clinic-api/src/main/java/com/example/clinicapi/repository/MedicoRepository.java
@@ -1,11 +1,22 @@
 package com.example.clinicapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.example.clinicapi.model.Especialidade;
 import com.example.clinicapi.model.Medico;
 
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
 	
+	// Listar apenas médicos ativos
+    @Query("SELECT m FROM Medico m WHERE m.ativo = true")
+    List<Medico> findAllAtivos();
+
+    // Buscar médicos ativos por especialidade
+    @Query("SELECT m FROM Medico m WHERE m.ativo = true AND m.especialidade = :especialidade")
+    List<Medico> findByEspecialidadeAndAtivoTrue(Especialidade especialidade);
 }
--- clinic-api/src/main/java/com/example/clinicapi/repository/PacienteRepository.java
+++ clinic-api/src/main/java/com/example/clinicapi/repository/PacienteRepository.java
@@ -1,6 +1,9 @@
 package com.example.clinicapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.clinicapi.model.Paciente;
@@ -8,4 +11,7 @@ import com.example.clinicapi.model.Paciente;
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 	
+	// Listar apenas pacientes ativos
+    @Query("SELECT p FROM Paciente p WHERE p.ativo = true")
+    List<Paciente> findAllAtivos();
 }
