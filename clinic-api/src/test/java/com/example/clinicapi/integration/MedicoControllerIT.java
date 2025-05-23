467fa384f276d1e895a8507d741d4c42ce989cab
Danilo Takeo <danilot.kanizawa@gmail.com>
2025-05-23 11:02:17 -0300

    Alteração do teste de integrarção de Medico para suportar Testcointainers MySQL ao invés de H2

--- clinic-api/src/test/java/com/example/clinicapi/integration/MedicoControllerIT.java
+++ clinic-api/src/test/java/com/example/clinicapi/integration/MedicoControllerIT.java
@@ -18,7 +18,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import com.example.clinicapi.dto.MedicoDTO;
 import com.example.clinicapi.model.Especialidade;
@@ -28,11 +33,31 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.transaction.Transactional;
 
+@Testcontainers
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @Transactional
 class MedicoControllerIT {
+	
+	@Container
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
+        .withDatabaseName("test_db")
+        .withUsername("testuser")
+        .withPassword("testpass");
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+	    registry.add("spring.datasource.url", mysql::getJdbcUrl);
+	    registry.add("spring.datasource.username", mysql::getUsername);
+	    registry.add("spring.datasource.password", mysql::getPassword);
+	    registry.add("spring.datasource.driver-class-name", mysql::getDriverClassName);
+	    registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.MySQL8Dialect");
+	    registry.add("spring.flyway.url", mysql::getJdbcUrl);
+	    registry.add("spring.flyway.user", mysql::getUsername);
+	    registry.add("spring.flyway.password", mysql::getPassword);
+	    registry.add("spring.flyway.enabled", () -> "true");
+	}
 
     @Autowired
     private MockMvc mockMvc;
